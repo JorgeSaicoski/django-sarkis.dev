811b12a86c63b3ec54acd4c6a4a9d76a32fa635b
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-01-06 16:48:11 -0300

    List projects

--- sarkis/projects/models.py
+++ sarkis/projects/models.py
@@ -8,7 +8,14 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     sub_services = models.ManyToManyField(SubService, related_name="sub_services")
-    skill = models.ManyToManyField(Skill, related_name="sub_services")
+    skill = models.ManyToManyField(Skill, related_name="skill")
 
     def __str__(self):
         return self.name
+
+    @property
+    def get_skills(self):
+        list = []
+        for i in self.skill.all():
+            list.append(i)
+        return list
--- sarkis/website/views.py
+++ sarkis/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from skills.models import Category, Service
+from projects.models import Project
 
 
 # Create your views here.
@@ -7,10 +8,12 @@ from skills.models import Category, Service
 def home(req):
     categories = Category.objects.all()
     services = Service.objects.all()
+    projects = Project.objects.all()
 
 
     context = {
         'categories': categories,
-        'services': services
+        'services': services,
+        'projects': projects
     }
     return render(req, 'website/home.html', context)
