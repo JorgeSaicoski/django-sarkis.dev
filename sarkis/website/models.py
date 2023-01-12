06b9aaa395cee864e85294f66f30988f9064f717
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-01-12 17:51:32 -0300

    Smooth

--- sarkis/website/models.py
+++ sarkis/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    # fields of the model
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254, default="")
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+    # renames the instances of the model
+    # with their title name
+    def __str__(self):
+        return self.title
