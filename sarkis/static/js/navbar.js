debfa83c36c2cc8b22fa3a455fb904c9c7c8ddbd
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-01-16 16:00:56 -0300

    scss improve

--- sarkis/static/js/navbar.js
+++ sarkis/static/js/navbar.js
@@ -1,10 +1,17 @@
 const navbar = document.getElementById("navbar")
+document.getElementById("close-navbar").style.display = "none";
+document.getElementById("menu-navbar").style.display = "";
 
 function changeOpen(){
     if (opened){
         navbar.classList.remove('navbar-active')
+        document.getElementById("close-navbar").style.display = "none";
+        document.getElementById("menu-navbar").style.display = "";
+
     }else{
         navbar.classList.add('navbar-active')
+        document.getElementById("close-navbar").style.display = "";
+        document.getElementById("menu-navbar").style.display = "none";
     }
     opened = !opened
 }
